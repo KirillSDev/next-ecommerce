89740b23c05aaa07620ba70f1ae06003f1664f98
Kirill Starodubsev <colereteps@gmail.com>
2023-01-02 15:50:46 +0400

    added color palette

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return (
+		<>
+			<Head>
+				<title>Rankings App</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;900&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
