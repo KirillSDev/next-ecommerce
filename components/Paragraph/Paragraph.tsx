3c545a47fb1c9fc43a85f538f17636a3301f4b07
Kirill Starodubsev <colereteps@gmail.com>
2023-01-02 22:49:59 +0400

    added button, paragraph component

---
+++ components/Paragraph/Paragraph.tsx
@@ -0,0 +1,23 @@
+import styles from './Paragraph.module.css';
+import { ParagraphProps } from './Paragraph.props';
+import cn from 'classnames';
+
+export const Paragraph = ({
+	size = 'medium',
+	children,
+	className,
+	...props
+}: ParagraphProps): JSX.Element => {
+	return (
+		<p
+			className={cn(styles.paragraph, {
+				[styles.small]: size == 'small',
+				[styles.middle]: size == 'medium',
+				[styles.big]: size == 'big',
+			})}
+			{...props}
+		>
+			{children}
+		</p>
+	);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
+import { Htag, Button, Paragraph } from '../components';
+
 export default function Home(): JSX.Element {
-	return <div>Home</div>;
+	return (
+		<>
+			<Htag tag="h1">Text</Htag>
+			<Button apperance="primary" arrow="down">
+				Log In
+			</Button>
+			<Button apperance="unprimary" arrow="right">
+				Register
+			</Button>
+			<Paragraph size="small">
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, illo
+				dolor eos possimus consequatur similique, corrupti earum ea autem
+				totam magnam natus, quisquam architecto maiores saepe doloremque
+				maxime dicta exercitationem!
+			</Paragraph>
+			<Paragraph size="medium">
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, illo
+				dolor eos possimus consequatur similique, corrupti earum ea autem
+				totam magnam natus, quisquam architecto maiores saepe doloremque
+				maxime dicta exercitationem!
+			</Paragraph>
+		</>
+	);
 }
