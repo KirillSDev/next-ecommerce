d693b4c720ace95f76e46d7aa670f2c1b22b5984
Kirill Starodubsev <colereteps@gmail.com>
2023-01-03 22:39:30 +0400

    added HOC component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Htag, Button, Paragraph, Tag, Rating } from '../components';
+import { wrapLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 	return (
 		<>
@@ -31,3 +32,4 @@ export default function Home(): JSX.Element {
 		</>
 	);
 }
+export default wrapLayout(Home);
