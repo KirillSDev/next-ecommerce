c2cd39ce9650d96a91686a8b46d7bf1b0e3b5fea
Kirill Starodubsev <colereteps@gmail.com>
2023-01-04 14:31:22 +0400

    responsive layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -7,14 +7,12 @@ import { Sidebar } from './Sidebar/Sidebar';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>{children}</div>
-			</div>
-			<Footer />
-		</>
+		<div className={styles.container}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.main}>{children}</div>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
 
